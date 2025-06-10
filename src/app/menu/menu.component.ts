5c7e404bb872e0a4d64138da7a537f64466081f1
Rafael Alves De Carvalho <rafaelalvesdecarvalho25@gmail.com>
2025-06-10 15:47:01 -0300

    fazendo menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { MenuService } from '../services/menu.service';
+import { Menu } from '../models/menu';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+
   constructor(private menuService: MenuService) { }
+
+  menuItems?: Array<Menu>;
+
+  menuStatus: string = 'closed';
+
+
+
+  ngOnInit() {
+    const menu = this.menuService.getItems();
+
+    this.menuItems = menu;
+  }
+
+  menuClick() {
+    this.menuStatus = (this.menuStatus === 'open' ? 'closed' : 'open')
+  }
 }
